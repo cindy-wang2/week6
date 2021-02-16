b6e8e9779be847c83cd823901f561a8431dc3d9d
Cindy Wang <cindy.wang2@kellogg.northwestern.edu>
2021-02-15 23:47:39 -0600

    added collection

--- homework/movies.js
+++ homework/movies.js
@@ -1,22 +1,6 @@
-// First, sign up for an account at https://themoviedb.org
-// Once verified and signed-in, go to Settings and create a new
-// API key; in the form, indicate that you'll be using this API
-// key for educational or personal use, and you should receive
-// your new key right away.
-
-// For this exercise, we'll be using the "now playing" API endpoint
-// https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US
-
-// Note: image data returned by the API will only give you the filename;
-// prepend with `https://image.tmdb.org/t/p/w500/` to get the 
-// complete image URL
 window.addEventListener('DOMContentLoaded', async function(event) {
   let db = firebase.firestore()
-  // Step 1: Construct a URL to get movies playing now from TMDB, fetch
-  // data and put the Array of movie Objects in a variable called
-  // movies. Write the contents of this array to the JavaScript
-  // console to ensure you've got good data
-  // ⬇️ ⬇️ ⬇️
+
   let url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=9b581f7d8c842c457d6c8baa24e27295&language=en-US'
   let response = await fetch(url)
   let movies = await response.json()
@@ -26,19 +10,7 @@ window.addEventListener('DOMContentLoaded', async function(event) {
   console.log(allMovies.length)
 
   // ⬆️ ⬆️ ⬆️ 
-  // End Step 1
-  
-  // Step 2: 
-  // - Loop through the Array called movies and insert HTML
-  //   into the existing DOM element with the class name .movies
-  // - Include a "watched" button to click for each movie
-  // - Give each "movie" a unique class name based on its numeric
-  //   ID field.
-  // Some HTML that would look pretty good... replace with real values :)
-  // <div class="w-1/5 p-4 movie-abcdefg1234567">
-  //   <img src="https://image.tmdb.org/t/p/w500/moviePosterPath.jpg" class="w-full">
-  //   <a href="#" class="watched-button block text-center text-white bg-green-500 mt-4 px-4 py-2 rounded">I've watched this!</a>
-  // </div>
+
   // ⬇️ ⬇️ ⬇️
       for(let i=0; i<allMovies.length; i++) {
         let movieID = allMovies[i].id
@@ -55,23 +27,19 @@ window.addEventListener('DOMContentLoaded', async function(event) {
           watchButton.addEventListener('click', async function(event){
               event.preventDefault()
               selectedMovie.classList.add('opacity-20')
+
+              await db.collection('watched').doc(`${movieID}`).set({})
+
+              
+
           })
       }
   
-     
-
-      //let querySnapshot = await db.collection('watched').get()
-       //  let watched = querySnapshot.docs
-        // for (let j=0; j < watched.length; j++){
-        //   let watched = watched[j].data()
-        //   watched.name
-       //  }
+    
      
         
 
-  
-  // ⬆️ ⬆️ ⬆️ 
-  // End Step 2
+
 
   // Step 3: 
   // - Attach an event listener to each "watched button"
